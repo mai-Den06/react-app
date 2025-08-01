414aa73978923eba8d3815c5cfadf9525796a560
mai-Den06 <anaaki.maiden@gmail.com>
2025-08-01 19:29:29 +0900

    add: ゲーム選択機能の追加

--- easy-todo/src/components/TodoList.tsx
+++ easy-todo/src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import "../App.css";
 
 function TodoList() {
   const [task, setTask] = useState("");
+  const [selectedGame, setSelectedGame] = useState("");
   const [todos, setTodos] = useState<string[]>([]);
 
   const handleAdd = () => {
@@ -18,12 +19,27 @@ function TodoList() {
           type="text"
           value={task}
           onChange={(e) => setTask(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleAdd();
+            }
+          }}
         />
+        <select
+          value={selectedGame}
+          onChange={(e) => setSelectedGame(e.target.value)}
+        >
+          <option value="">--選択--</option>
+          <option value="原神">原神</option>
+          <option value="崩壊SR">崩壊SR</option>
+          <option value="ZZZ">ZZZ</option>
+          <option value="シャドバ">シャドバ</option>
+        </select>
         <button onClick={handleAdd}>追加</button>
       </div>
       <div>
         {todos.map((todo, index) => (
-          <Todo key={index} task={todo} />
+          <Todo key={index} task={todo} game={selectedGame} />
         ))}
       </div>
     </>
@@ -34,13 +50,15 @@ export default TodoList;
 
 type TodoProps = {
   task: string;
+  game?: string;
 };
 
-function Todo({ task }: TodoProps) {
+function Todo({ task, game }: TodoProps) {
   return (
     <div className="todo-item">
       <input type="checkbox" />
       <span>{task}</span>
+      {game && <span className="game-label">[{game}]</span>}
       <button>削除</button>
     </div>
   );
