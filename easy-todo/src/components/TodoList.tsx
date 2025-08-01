08f1d6057367198288604c0af574b6dbe713c451
mai-Den06 <anaaki.maiden@gmail.com>
2025-08-01 18:29:24 +0900

    add: タスク追加機能の追加

--- easy-todo/src/components/TodoList.tsx
+++ easy-todo/src/components/TodoList.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import "../App.css";
 
 function TodoList() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const handleAdd = () => {
+    if (task.trim() === "") return;
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
   return (
     <>
       <div>
-        <input type="text" />
-        <button>追加</button>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={handleAdd}>追加</button>
       </div>
       <div>
-        <Todo />
+        {todos.map((todo, index) => (
+          <Todo key={index} task={todo} />
+        ))}
       </div>
     </>
   );
@@ -16,11 +32,15 @@ function TodoList() {
 
 export default TodoList;
 
-function Todo() {
+type TodoProps = {
+  task: string;
+};
+
+function Todo({ task }: TodoProps) {
   return (
     <div className="todo-item">
       <input type="checkbox" />
-      <span>やること</span>
+      <span>{task}</span>
       <button>削除</button>
     </div>
   );
